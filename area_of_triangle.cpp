73a14d38a0dfa74f6b5754bd0696357eb8243690
rvgarg <raghav.garg175@gmail.com>
2019-07-24 15:25:57 +0530

    first programms

---
+++ area_of_triangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int height,base;
+    double area;
+    cout<<"enter height ";
+    cin>>height;
+    cout<<"enter base ";
+    cin>>base;
+    area = (base*height)/2;
+    cout<<endl<<"area is "<<area;
+
+}
